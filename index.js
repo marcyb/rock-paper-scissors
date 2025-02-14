d4ec73d0a55ca5e2ac016e58ec30837459af6c35
Marc Bleeze <marcyb@users.noreply.github.com>
2025-02-14 14:26:50 +0000

    Complete step 2

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello, World!");
+function getComputerChoice() {
+  const choice = Math.random();
+  if (choice < 0.34) return 'rock';
+  if (choice < 0.67) return 'paper';
+  return 'scissors';
+}
+
+console.log(getComputerChoice());
